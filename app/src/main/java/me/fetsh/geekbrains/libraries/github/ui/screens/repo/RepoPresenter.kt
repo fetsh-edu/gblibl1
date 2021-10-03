5a8ba07c937d9e3f24f95fbb5f6ae1e80d14cfae
fetsh-edu <fetsh.me@gmail.com>
2021-10-03 20:47:13 +0300

    Revert "Move interpolator to constructor"
    This reverts commit 1ee53b9d01a95c4a36b5bc4e786b668740a3ccad.

--- app/src/main/java/me/fetsh/geekbrains/libraries/github/ui/screens/repo/RepoPresenter.kt
+++ app/src/main/java/me/fetsh/geekbrains/libraries/github/ui/screens/repo/RepoPresenter.kt
@@ -7,18 +7,20 @@ import me.fetsh.geekbrains.libraries.github.models.RepoInterpolator
 import moxy.MvpPresenter
 import javax.inject.Inject
 
-class RepoPresenter
+class RepoPresenter @Inject constructor(
+    private val router: Router,
+    private val repo: GithubRepoUI
+) : MvpPresenter<RepoView>() {
+
     @Inject
-    constructor(
-        private val router: Router,
-        private val repo: GithubRepoUI,
-        private val repoInterpolator: RepoInterpolator
-    ) : MvpPresenter<RepoView>() {
+    lateinit var repoInterpolator : RepoInterpolator
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init(repo)
-        Log.wtf("Interpolated", "${repoInterpolator.interpolate(repo)}")
+        if (this::repoInterpolator.isInitialized) {
+            Log.wtf("Interpolated", "${repoInterpolator.interpolate(repo)}")
+        }
     }
 
     fun backPressed(): Boolean {
